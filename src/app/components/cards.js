8b5eabee448adacb86145d30c5256ccb4a3348c0
saamonlover <saamonlover@gmail.com>
2024-08-21 02:19:31 -0700

    feat: allow card to get expanded from its original position smoothly

--- src/app/components/cards.js
+++ src/app/components/cards.js
@@ -1,24 +1,53 @@
 'use client'
 
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+
+const Card = ({ color, onClick, handleClick }) => (
+  <motion.div
+    whileHover={{ scale: 1.1 }}
+    drag
+    dragConstraints={{
+      top: -5,
+      left: -5,
+      right: 5,
+      bottom: 5,
+    }}
+    className='w-16 h-20 rounded-lg cursor-pointer'
+    style={{ backgroundColor: color }}
+    onClick={color !== '#B43F3F' ? onClick : handleClick}
+    layoutId={`card-${color}`}
+  ></motion.div>
+)
+
 const Cards = () => {
   const colors = ['#FF8225', '#B43F3F', '#173B45']
-
-  const handleClick = (color) => {
-    alert(`Card with color ${color} clicked!`)
-  }
+  const [selected, setSelected] = useState(null)
 
   return (
-    <div className='mt-8'>
-      <div className='flex justify-between gap-4'>
-        {colors.map((color, index) => (
-          <div
-            key={index}
-            className='w-16 h-20 rounded-lg cursor-pointer'
-            style={{ backgroundColor: color }}
-            onClick={() => handleClick(color)}
-          ></div>
-        ))}
-      </div>
+    <div className='mt-8 w-full'>
+      <motion.div
+        layout
+        transition={{ layout: { duration: 1, type: 'spring' } }}
+      >
+        {!selected ? (
+          <div className='flex justify-center gap-4'>
+            {colors.map((color, index) => (
+              <Card
+                key={index}
+                color={color}
+                onClick={() => setSelected(color)}
+              />
+            ))}
+          </div>
+        ) : selected !== '#B43F3F' ? (
+          <motion.div
+            className='h-64 rounded-lg mx-auto'
+            style={{ backgroundColor: selected, width: '25rem' }}
+            layoutId={`card-${selected}`}
+          ></motion.div>
+        ) : null}
+      </motion.div>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Cards from '../app/components/cards'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col justify-center items-center p-24'>
+    <main className='flex min-h-screen flex-col justify-center items-center'>
       <h1 className='text-4xl font-bold'>You can call me Ricky</h1>
       <h1 className='text-2xl font-bold text-zinc-600'>yccmr</h1>
       <Cards />
