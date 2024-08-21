e5d1629871debabcb0e1611f72aa1e30261fdfc5
saamonlover <saamonlover@gmail.com>
2024-08-21 01:06:08 -0700

    feat: add welcome message and init main content component which is called Cards

---
+++ src/app/components/cards.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Cards = () => {
+  return <div>{/* Add your card content here */}</div>
+}
+
+export default Cards
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import Cards from '../app/components/cards'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className='flex min-h-screen flex-col justify-center items-center p-24'>
+      <h1 className='text-4xl font-bold'>You can call me Ricky</h1>
+      <h1 className='text-2xl font-bold text-zinc-600'>yccmr</h1>
+      <Cards />
+    </main>
   )
 }
