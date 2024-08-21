aaf546644a2c156d60077caf2bd6336513579f9d
saamonlover <saamonlover@gmail.com>
2024-08-21 01:14:33 -0700

    feat: add card snippets which represent different expandable sections

--- src/app/components/cards.js
+++ src/app/components/cards.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
 const Cards = () => {
-  return <div>{/* Add your card content here */}</div>
+  const colors = ['#FF8225', '#B43F3F', '#173B45']
+
+  return (
+    <div className='mt-8'>
+      <div className='flex justify-between gap-4'>
+        {colors.map((color, index) => (
+          <div
+            key={index}
+            className='w-16 h-20 rounded-lg'
+            style={{ backgroundColor: color }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Cards
