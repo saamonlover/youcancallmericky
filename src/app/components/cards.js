7397b7d3718577fac3f33fb5af3f1bf745a4879a
saamonlover <saamonlover@gmail.com>
2024-08-21 01:21:29 -0700

    build: convert card elements to clickable ones and install framer motion to enable card expansion animation

--- src/app/components/cards.js
+++ src/app/components/cards.js
@@ -1,7 +1,11 @@
-import React from 'react'
+'use client'
 
 const Cards = () => {
   const colors = ['#FF8225', '#B43F3F', '#173B45']
+
+  const handleClick = (color) => {
+    alert(`Card with color ${color} clicked!`)
+  }
 
   return (
     <div className='mt-8'>
@@ -9,8 +13,9 @@ const Cards = () => {
         {colors.map((color, index) => (
           <div
             key={index}
-            className='w-16 h-20 rounded-lg'
+            className='w-16 h-20 rounded-lg cursor-pointer'
             style={{ backgroundColor: color }}
+            onClick={() => handleClick(color)}
           ></div>
         ))}
       </div>
